8b4e1ec46366213c6c3405803eb6330f55d82f86
Stephan <stephan.gu@edu.turkuamk.fi>
2022-12-11 14:31:48 +0200

    Added reservation.py to resources and schemas

---
+++ resources/reservation.py
@@ -0,0 +1,154 @@
+from flask import request
+from flask_restful import Resource
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from http import HTTPStatus
+
+from models.reservation import Reservation
+from schemas.reservation import ReservationSchema
+
+from marshmallow import ValidationError
+
+reservation_schema = ReservationSchema()
+reservation_list_schema = ReservationSchema(many=True)
+
+
+class ReservationListResource(Resource):
+
+    def get(self):
+
+        reservation = Reservation.get_all_published()
+
+        return reservation_list_schema.dump(reservation), HTTPStatus.OK
+
+    @jwt_required()
+    def post(self):
+        json_data = request.get_json()
+        current_user = get_jwt_identity()
+
+        try:
+            data = reservation_schema.load(data=json_data)
+        except ValidationError as err:
+            errors = err.messages
+            return {'message': 'Validation errors', 'errors': errors}, HTTPStatus.BAD_REQUEST
+
+        reservation = Reservation(**data)
+        reservation.user_id = current_user
+        reservation.save()
+
+        return reservation_schema.dump(reservation), HTTPStatus.CREATED
+
+
+class ReservationResource(Resource):
+
+    @jwt_required(optional=True)
+    def get(self, reservation_id):
+
+        reservation = Reservation.get_by_id(reservation_id=reservation_id)
+
+        if reservation is None:
+            return {'message': 'reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if reservation.is_publish == False and reservation.user_id != current_user:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        return reservation.data(), HTTPStatus.OK
+
+    @jwt_required()
+    def patch(self, reservation_id):
+
+        json_data = request.get_json()
+
+        try:
+            data = reservation_schema.load(data=json_data)
+        except ValidationError as err:
+            errors = err.messages
+            return {'message': 'Validation errors', 'errors': errors}, HTTPStatus.BAD_REQUEST
+
+        reservation =Reservation.get_by_id(reservation_id=reservation_id)
+
+        if reservation is None:
+            return {'message': 'Reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != reservation.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        reservation.user_id = data.get('user_id') or reservation.user_id
+        reservation.workspace_id = data.get('workspace_id') or reservation.workspace_id
+        reservation.date = data.get('date') or reservation.date
+
+        reservation.save()
+
+        return reservation.dump(reservation), HTTPStatus.OK
+
+    @jwt_required()
+    def put(self, reservation_id):
+
+        json_data = request.get_json()
+
+        reservation = Reservation.get_by_id(reservation_id=reservation_id)
+
+        if reservation is None:
+            return {'message': 'reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != reservation.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        reservation.user_id = json_data['user_id']
+        reservation.workspace_id = json_data['workspace_id']
+        reservation.date = json_data['date']
+
+        reservation.save()
+
+        return reservation.data(), HTTPStatus.OK
+
+    @jwt_required()
+    def delete(self, reservation_id):
+        reservation = Reservation.get_by_id(reservation_id=reservation_id)
+
+        if reservation is None:
+            return {'message': 'reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != reservation.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        reservation.delete()
+
+        return {}, HTTPStatus.NO_CONTENT
+
+
+class ReservationPublishResource(Resource):
+
+    @jwt_required()
+    def put(self, reservation_id):
+        reservation = Reservation.get_by_id(reservation_id=reservation_id)
+
+        if reservation is None:
+            return {'message': 'reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != reservation.user_id:
+            return {'message': 'Access denied!'}, HTTPStatus.FORBIDDEN
+
+        reservation.is_publish = True
+        reservation.save()
+
+        return {}, HTTPStatus.NO_CONTENT
+
+    def delete(self, reservation_id):
+        reservation = next((reservation for reservation in reservation_list if reservation.id == reservation_id), None)
+
+        if reservation is None:
+            return {'message': 'reservation not found'}, HTTPStatus.NOT_FOUND
+
+        reservation.is_publish = False
+
+        return {}, HTTPStatus.NO_CONTENT
---
+++ schemas/reservation.py
@@ -0,0 +1,28 @@
+from marshmallow import Schema, fields, post_dump, validate, validates, ValidationError
+from schemas.user import UserSchema
+
+
+class ReservationSchema(Schema):
+    class Meta:
+        ordered = True
+
+    id = fields.Integer(dump_only=True)
+    user_id = fields.Integer(dump_only=True)
+    workspace_id = fields.Integer(dump_only=True)
+    date = fields.DateTime(dump_only=True)
+    
+
+    is_publish = fields.Boolean(dump_only=True)
+
+    author = fields.Nested(UserSchema, attribute='user', dump_only=True, only=['id', 'username'])
+
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+
+
+    @post_dump(pass_many=True)
+    def wrap(self, data, many, **kwargs):
+        if many:
+            return {'data': data}
+        return data
+
