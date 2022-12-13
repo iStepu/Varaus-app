9d876c0db122ee29a0dc6b55a251f7d5cb6f1416
Antti Tikka <antti.tikka@hotmail.com>
2022-12-13 22:53:06 +0200

    Lisätty mahdollisuus muokata ja poistaa työtila

--- resources/workspace.py
+++ resources/workspace.py
@@ -1,9 +1,10 @@
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from marshmallow import ValidationError
 from http import HTTPStatus
 
+from models.user import User
 from models.workspace import Workspace
 from schemas.workspace import WorkspaceSchema
 
@@ -52,3 +53,48 @@ class WorkspaceResource(Resource):
         data = workspace_schema.dump(workspace)
 
         return data, HTTPStatus.OK
+
+    @jwt_required()
+    def patch(self, workspace_id):
+        json_data = request.get_json()
+
+        try:
+            data = workspace_schema.load(data=json_data, partial=True)
+        except ValidationError as err:
+            errors = err.messages
+            return {'message': 'Validation errors', 'errors': errors}, HTTPStatus.BAD_REQUEST
+
+        workspace = Workspace.get_by_workspace_id(workspace_id)
+
+        if workspace is None:
+            return {'message': 'Workspace not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = User.get_by_id(get_jwt_identity())
+
+        if not current_user.is_admin:
+            return {'message': 'Access is not allowed, current user is not admin'}, HTTPStatus.FORBIDDEN
+
+        workspace.name = data.get('name') or workspace.name
+        workspace.campus = data.get('campus') or workspace.campus
+        workspace.building = data.get('building') or workspace.building
+        workspace.description = data.get('description') or workspace.description
+
+        workspace.save()
+
+        return workspace_schema.dump(workspace), HTTPStatus.OK
+
+    @jwt_required()
+    def delete(self, workspace_id):
+        workspace = Workspace.get_by_workspace_id(workspace_id)
+
+        if not workspace:
+            return {"message": "workspace not found"}, HTTPStatus.NOT_FOUND
+
+        current_user = User.get_by_id(get_jwt_identity())
+
+        if not current_user.is_admin:
+            return {'message': 'Access is not allowed, current user is not admin'}, HTTPStatus.FORBIDDEN
+
+        workspace.delete()
+
+        return {}, HTTPStatus.NO_CONTENT
